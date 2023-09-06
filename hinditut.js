9cfba1e9961159cce770b602540297f0d612de69
vipi991 <vipi.sharma23@gmail.com>
2023-09-06 10:50:28 +0530

    Update hinditut.js

--- hinditut.js
+++ hinditut.js
@@ -1,4 +1,3 @@
-<script type="text/javascript">
 if (!localStorage.getItem('cookieConsent')) {
   document.body.innerHTML += '<div style="position:fixed;bottom:0;left:0;width:100%;background:#333;color:#fff;padding:10px;text-align:center;z-index:9999;">इस वेबसाइट पर कुकीज़ का उपयोग होता है। <a style="color:#fff;text-decoration:underline;cursor:pointer;" id="acceptCookies">स्वीकार करें</a></div>';
 document.getElementById('acceptCookies').onclick = function() {
@@ -14,4 +13,3 @@ function handleScroll() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("HTML1").style.width = scrolled + "%";
 }
-</script>
